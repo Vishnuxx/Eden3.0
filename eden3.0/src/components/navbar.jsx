2544175574101b74a575446c0c4c12f60ff19c67
vishnuxx <vishnuvs2002it@gmail.com>
2024-04-01 00:52:56 +0530

    update

--- eden3.0/src/components/navbar.jsx
+++ eden3.0/src/components/navbar.jsx
@@ -18,22 +18,22 @@ function NavBar() {
 		{ text: "Agenda", url: "#agenda" },
 	];
 	return (
-		<nav className="flex flex-row justify-between h-[90px] w-full z-50 text-white shadow-sm fixed top-0 left-0 md:px-20 ">
+		<nav className="flex flex-row justify-between h-[90px] w-full z-50 text-white shadow-sm fixed top-0 left-0 md:px-20 bg-gradient-to-b from-[#000000] to-transparant transition-all duration-300">
 			<div className="flex flex-row w-fit items-center">
 				<Link scroll={true} href={"#"}>
 					<img src={logo} className="w-[200px] h-[70px] object-cover" alt="logo" />
 				</Link>
 			</div>
 			{isOpen && (
-				<div className="md:hidden">
-					<div onClick={openDrawer} className="w-full h-screen top-0 left-0 md:hidden bg-black absolute bg-opacity-20"></div>
+				<div className="md:hidden flex flex-col items-center justify-center ">
+					<div onClick={openDrawer} className="w-full h-screen top-0 left-0 md:hidden bg-black absolute bg-opacity-[0.8] backdrop:blur-lg"></div>
 					<div className="flex flex-col border border-[#FF9F2F] rounded-xl  w-full shadow-lg h-fit min-h-[50%] fixed top-0 left-0 gap-0 py-10 justify-center items-center">
 						<div className="flex md:hidden flex-row w-fit items-center">
 							<img src={logo} className="w-[200px] h-[70px] object-cover" alt="logo" />
 							{/* <h4 className="font-bold text-3xl">Matrix</h4> */}
 						</div>
 						{menuItems.map((value, index) => (
-							<Link to={value.url} onClickCapture={openDrawer} className="text-md text-center  border-b border-gray md:border-0 p-4 py-6 md:p-0  flex items-center justify-center h-full cursor-pointer w-full hover:bg-gray md:hover:bg-[#ffffff00] md:hover:text-primary" key={index} onClick={() => navigate(value.url)}>
+							<Link to={value.url} onClickCapture={openDrawer} className="text-md text-center hover:text-orange-300 md:border-0 p-4 py-6 md:p-0  flex items-center justify-center h-full cursor-pointer w-full hover:bg-gray md:hover:bg-[#ffffff00] md:hover:text-primary" key={index} onClick={() => navigate(value.url)}>
 								{value.text}
 							</Link>
 						))}
@@ -43,7 +43,7 @@ function NavBar() {
 
 			<div className=" flex-row hidden md:flex w-full max-w-[600px] justify-around items-center">
 				{menuItems.map((value, index) => (
-					<Link to={value.url} onClickCapture={openDrawer} className="text-md  text-center  border-b border-gray md:border-0 p-4 py-6 md:p-0  flex items-center justify-center h-full cursor-pointer w-full hover:bg-gray md:hover:bg-[#ffffff00] md:hover:text-primary" key={index} onClick={() => navigate(value.url)}>
+					<Link to={value.url} onClickCapture={openDrawer} className="text-md hover:text-orange-300 text-center  border-b border-gray md:border-0 p-4 py-6 md:p-0  flex items-center justify-center h-full cursor-pointer w-full hover:bg-gray md:hover:bg-[#ffffff00] md:hover:text-primary" key={index} onClick={() => navigate(value.url)}>
 						{value.text}
 					</Link>
 				))}
