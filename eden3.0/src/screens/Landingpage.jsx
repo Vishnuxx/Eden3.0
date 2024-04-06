786532ad3580f842f0dc430a77a71c3043e885a9
vishnuxx <vishnuvs2002it@gmail.com>
2024-04-06 22:23:35 +0530

    changed navbar to navigation bar

--- eden3.0/src/screens/Landingpage.jsx
+++ eden3.0/src/screens/Landingpage.jsx
@@ -1,12 +1,14 @@
 import AboutPage from "./About.Page";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
+import NavBar from "../components/NavigationBar";
 
 
 
 function LandingPage() {
     return (
         <div className="w-full h-full overflow-y-scroll scroll-smooth">
+            <NavBar></NavBar>
             <HeroSection />
             <AboutPage></AboutPage>
             <Footer />
