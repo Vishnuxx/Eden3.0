c639d93736195ec48560e34694a8883e6ff63d8e
vishnuxx <vishnuvs2002it@gmail.com>
2024-04-06 22:17:06 +0530

    update import

--- eden3.0/src/screens/Landingpage.jsx
+++ eden3.0/src/screens/Landingpage.jsx
@@ -1,7 +1,8 @@
-import NavBar from "../components/NavBar";
 import AboutPage from "./About.Page";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
+import NavBar from "../components/navbar";
+
 
 function LandingPage() {
     return (
@@ -17,36 +18,3 @@ function LandingPage() {
 export default LandingPage;
 		
 
-
-const AnimatedButton = () => {
-    const [buttonState, setButtonState] = useState(0);
-
-    const handleClick = () => {
-        setButtonState(2);
-        // console.log("click");
-    };
-
-    const onPointerEnter = () => {
-        setButtonState(2);
-        // console.log("enter");
-    };
-
-    const onPointerExit = () => {
-        setButtonState(0);
-        // console.log("exit");
-    };
-
-    const onPointerUp = () => {
-        setButtonState(0)
-        
-        // console.log("up");
-    };
-
-    return (
-        <a href="" onPointerEnter={onPointerEnter} onPointerLeave={onPointerExit} onPointerDown={handleClick} onPointerUp={onPointerUp} className=" relative z-[2] outline-none flex items-center box-border">
-            {buttonState == 0 && <img src={normalButton} alt="Button" className="touch-none pointer-events-none" />}
-            {buttonState == 1 && <img src={hoveredButton} alt="Button" className="touch-none pointer-events-none" />}
-            {buttonState == 2 && <img src={pressedButton} alt="Button" className="touch-none pointer-events-none" />}
-        </a>
-    );
-};
